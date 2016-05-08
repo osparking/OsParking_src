ea716f142b9a9bb83d2a6409eed41c0b12da7488
OsParking <osparking@users.noreply.github.com>
2016-05-09 07:32:28 +0900

    Update BlackFlyManager.java
    Description, in English and in Korean, on why this file doesn't have content added.

---
+++ OsParking/src/main/java/com/osparking/osparking/device/BlackFly/BlackFlyManager.java
@@ -0,0 +1,32 @@
+/* 
+First of all, sorry to bothering you downloaders. It is that the real contents of this file has been removed 
+to collect industry's attention on this software.
+So, if you wanted to do some research on this software or build, run it, please, send following information 
+to the author Dr. Park thru e-mail.
+
+  1. Your name,
+  2. Your e-mail address,
+  3. Your company name, (in case you're not affliated with a company, replace this with your cell phone number 
+     so I can send you a text and make sure you are a real programmer.)
+  4. Your nationality. 
+  
+After you get real/original BlackFlyManager.java file from the author, you may replace it with this file.
+Thanks.
+- Dr. Park, Jongbum
+- Email: jbpark03@gmail.com
+*/
+
+/* Korean translation follows */
+/* 먼저 불편을 드려 죄송합니다. 사과드리는 것은 업계의 관심도를 측정하기 위하여 이 파일의 내용이 삭제되었기 때문입니다. 
+그래서 프로그래머께서 다음 정보를 저자(박종범씨)에게 이메일로 다음 정보를 전송하시면 본래 파일을 전송해 드리겠습니다.
+
+  1. 당신의 성명
+  2. 이메일 주소
+  3. 소속사 이름(소속사가 없는 경우, 당신의 스파트폰 번호 (그 번호로 문자를 보내어 진짜 프로그래머인지 확인할 것임.)
+  4. 당신의 국적
+  
+당신이 원래 BlackFlyManager.java 파일을 받게 되면 이 파일을 받은 파일로 교체하시면 됩니다.
+감사합니다.
+- 저자 박종범 박사
+- 저자 이메일 jbpark03@gmail.com
+*/
